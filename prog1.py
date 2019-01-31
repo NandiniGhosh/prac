d15fe6b004e460fbaa68f75c20873be362abc677
NandiniGhosh <nandinighosh3008@gmail.com>
2019-01-31 18:38:54 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,31 @@
+def mostFrequent(arr, n):
+
+    arr.sort()
+
+    max_count = 1;
+    res = arr[0];
+    curr_count = 1
+
+    for i in range(1, n):
+        if (arr[i] == arr[i - 1]):
+            curr_count += 1
+
+        else:
+            if (curr_count > max_count):
+                max_count = curr_count
+                res = arr[i - 1]
+
+            curr_count = 1
+
+    # If last element is most frequent
+    if (curr_count > max_count):
+        max_count = curr_count
+        res = arr[n - 1]
+
+    return res
+
+
+# Driver Code
+arr = [1, 5, 5, 1, 3, 5, 2, 1]
+n = len(arr)
+print(mostFrequent(arr, n))
